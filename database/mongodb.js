1e3920b4b1dc15faeec1950f45b16b70caa92146
Vincentius Agung Prabandaru <vap.agung@gmail.com>
2023-05-22 17:32:09 +0700

    Ubah koneksi database dan Custom Message Regex

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGO_DB)
+  .connect('mongodb+srv://Bruh:Bruh@testing-patuli.ocvgxw3.mongodb.net/')
   .then(() => console.log('Database Connected'))
   .catch((error) => console.log(error.message));
