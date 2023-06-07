0710910b90f1da7bcfd325eb47b07e493fe7abec
imamalif1616 <imamalif1616@gmail.com>
2023-06-07 15:37:26 +0700

    not fix yet, error handler old token

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
 const httpStatus = require('http-status');
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+
 const Account = require('../model/Account');
 const Response = require('../model/Response');
 const clearToken = require('../utils/clearToken');
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const tokenRevocation = require('../utils/tokenRevocation');
 
 const secretmanagerClient = new SecretManagerServiceClient();
 
@@ -20,26 +22,37 @@ const callAccessSecretVersion = async () => {
 }
 
 const Auth = async (req, res, next) => {
-  const token = req.headers.authorization;
-  const response = new Response.Error(true, 'Unauthorized');
-
-  if (!token) {
-    res.status(httpStatus.UNAUTHORIZED).json(response);
-    return;
-  }
-
-  const myToken = clearToken(token);
-
-  jwt.verify(myToken, await callAccessSecretVersion(), async (error, payload) => {
-    if (error) {
+  try {
+    const token = req.headers.authorization;
+    const response = new Response.Error(true, 'Unauthorized');
+  
+    if (!token) {
       res.status(httpStatus.UNAUTHORIZED).json(response);
       return;
     }
-    const { id } = payload;
-    const account = await Account.findOne({ _id: id });
-    req.currentUser = account;
-    next();
-  });
-};
+  
+    const myToken = clearToken(token);
+  
+    if (await tokenRevocation.isTokenRevoked(token)) {
+      res.status(httpStatus.UNAUTHORIZED).json(response);
+      return;
+    }
+  
+    jwt.verify(myToken, await callAccessSecretVersion(), async (error, payload) => {
+      if (error) {
+        res.status(httpStatus.UNAUTHORIZED).json(response);
+        return;
+      }
+      
+      const { id } = payload;
+      const account = await Account.findOne({ _id: id });
+      req.currentUser = account;
+      next();
+    });
+  } catch (error) {
+    const response = new Response.Error(true, 'Internal server error');
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).json(response);
+  }
+}
 
 module.exports = Auth;
